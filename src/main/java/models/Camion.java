ee13f170ae0e0541f3c7fe9c83b0a0b5827b6a69
c04o <c04o@proton.me>
2025-09-20 16:00:25 -0600

    Camion uses Combustible

--- src/main/java/models/Camion.java
+++ src/main/java/models/Camion.java
@@ -1,16 +1,42 @@
 package models;
 
-public class Camion extends Vehiculo {
+import interfaces.Combustible;
+
+public class Camion extends Vehiculo implements Combustible {
     private double capacidadCarga;
+    private double nivelCombustible;
+    private static final String TIPO_COMBUSTIBLE = "Diésel";
 
     public Camion(String marca, String modelo, double capacidadCarga) {
         super(marca, modelo);
         this.capacidadCarga = capacidadCarga;
+        this.nivelCombustible = 0;
     }
 
     @Override
     public void mover() {
-        System.out.println("El camión " + marca + " " + modelo + " transporta carga pesada por la autopista.");
+        if (nivelCombustible > 0) {
+            System.out.println("El camión " + marca + " " + modelo + " transporta carga pesada por la autopista.");
+            nivelCombustible -= 10;
+        } else {
+            System.out.println("El camión " + marca + " " + modelo + " no puede moverse sin combustible.");
+        }
+    }
+
+    @Override
+    public void recargar(double cantidad) {
+        nivelCombustible += cantidad;
+        System.out.println("Recargando " + cantidad + " litros de " + TIPO_COMBUSTIBLE +
+                " en el camión " + marca + " " + modelo);
+    }
+
+    @Override
+    public String getTipoCombustible() {
+        return TIPO_COMBUSTIBLE;
+    }
+
+    public double getNivelCombustible() {
+        return nivelCombustible;
     }
 
     public void cargarMercancia() {
