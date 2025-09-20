d417c14dc797eecc640efd130c396450effa954d
c04o <c04o@proton.me>
2025-09-20 15:43:58 -0600

    Use override methods in Main

--- src/main/java/run/Main.java
+++ src/main/java/run/Main.java
@@ -1,8 +1,28 @@
-import models.Automovil;
+import models.Vehiculo;
+import models.Carro;
+import models.Moto;
+import models.Camion;
 
 public class Main {
     public static void main(String[] args) {
-        Automovil auto = new Automovil("Toyota", "Corolla");
-        auto.mover();
+        // Crear instancias de cada tipo de vehículo
+        Carro carro = new Carro("Toyota", "Corolla");
+        Moto moto = new Moto("Yamaha", "MT-07");
+        Camion camion = new Camion("Volvo", "FH16", 20.5);
+
+        // Usar polimorfismo con la clase base Vehiculo
+        Vehiculo[] vehiculos = {carro, moto, camion};
+
+        // Probar el método mover() en cada vehículo
+        for (Vehiculo vehiculo : vehiculos) {
+            vehiculo.mover();
+        }
+
+        System.out.println("\n--- Métodos específicos de cada subclase ---");
+
+        // Métodos específicos de cada subclase
+        carro.abrirMaletero();
+        moto.hacerCaballito();
+        camion.cargarMercancia();
     }
 }
