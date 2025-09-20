6153370f5487e4df1b2b52911788fdf622a0bf7b
c04o <c04o@proton.me>
2025-09-20 15:59:58 -0600

    Carro uses Combustible

--- src/main/java/models/Carro.java
+++ src/main/java/models/Carro.java
@@ -1,14 +1,40 @@
 package models;
 
-public class Carro extends Vehiculo {
+import interfaces.Combustible;
+
+public class Carro extends Vehiculo implements Combustible {
+    private double nivelCombustible;
+    private static final String TIPO_COMBUSTIBLE = "Gasolina";
 
     public Carro(String marca, String modelo) {
         super(marca, modelo);
+        this.nivelCombustible = 0;
     }
 
     @Override
     public void mover() {
-        System.out.println("El carro " + marca + " " + modelo + " se desplaza por la carretera.");
+        if (nivelCombustible > 0) {
+            System.out.println("El carro " + marca + " " + modelo + " se desplaza por la carretera.");
+            nivelCombustible -= 5;
+        } else {
+            System.out.println("El carro " + marca + " " + modelo + " no puede moverse sin combustible.");
+        }
+    }
+
+    @Override
+    public void recargar(double cantidad) {
+        nivelCombustible += cantidad;
+        System.out.println("Recargando " + cantidad + " litros de " + TIPO_COMBUSTIBLE +
+                " en el carro " + marca + " " + modelo);
+    }
+
+    @Override
+    public String getTipoCombustible() {
+        return TIPO_COMBUSTIBLE;
+    }
+
+    public double getNivelCombustible() {
+        return nivelCombustible;
     }
 
     public void abrirMaletero() {
