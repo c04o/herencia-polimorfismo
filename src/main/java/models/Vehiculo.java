54f0c9c4194bc1c11ec120567169314080d342d9
c04o <c04o@proton.me>
2025-09-20 15:36:27 -0600

    Add Vehiculo class

---
+++ src/main/java/models/Vehiculo.java
@@ -0,0 +1,13 @@
+package models;
+
+public abstract class Vehiculo {
+    protected String marca;
+    protected String modelo;
+
+    public Vehiculo(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public abstract void mover();
+}
